4754c38b2a6ee8214dea4f122bdb9241c82f7097
jessica garcia <93187222+JG38@users.noreply.github.com>
2024-03-03 17:14:58 -0500

    first commit

---
+++ Day3_wk2_Hw.py
@@ -0,0 +1,44 @@
+
+cart = {
+    'eggs': 6,
+    'bread': 2,
+}
+
+def show():
+    if len(cart) == 0:
+        print('Your cart is empty')
+    print(cart)
+    for k, v in cart.items():
+        print(f'There are {v} {k} in your cart')
+
+def add():
+    item = input('What item do you want to add?')
+    quantity = input('How many do you want to buy?')
+    cart[item] = int(quantity)
+    print(f'You now have {quantity} {item} in your cart')
+
+def delete():
+    item = input('What item do you want to remove?')
+    if item in cart:
+        del cart[item]
+        print(f'{item} has been removed from your cart')
+    else:
+        print(f'{item} is not in your cart')
+def clear():
+    cart={}
+    print('Your cart is now empty')
+
+    def runner()
+        menu = input('What do you want to do? Show/Add/Delete/Clear/Quit')
+        if menu.lower() == 'show':
+            show()
+        elif menu.lower() == 'add':
+            add()
+        elif menu.lower() == 'delete':
+            delete()
+        elif menu.lower() == 'clear':
+            clear()
+        elif menu.lower() == 'quit':
+            break
+        else:
+            print('Please try again')
